f6b384c38cad07504f9c799043d7e321cdd4c330
Camila <114501675+camilahess@users.noreply.github.com>
2022-10-20 17:44:37 +0200

    ejercicio1Funciones

---
+++ funciones/src/funciones/EjerciciosFunciones.java
@@ -0,0 +1,25 @@
+package funciones;
+
+public class EjerciciosFunciones {
+	
+	public static void ejercicio1() {
+		for(int i = 0; i <=20; i++) {
+			System.out.print("*");
+		} 
+		System.out.println();
+	}
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		ejercicio1();
+		ejercicio1();
+		ejercicio1();
+		
+
+	}
+
+}
