deae395046807c31f5de65ec0b8b8919e96d5339
Camila <114501675+camilahess@users.noreply.github.com>
2023-01-25 11:36:33 +0100

    Más sobre redimensiones

--- colecciones/src/colecciones/EjemplosArrays.java
+++ colecciones/src/colecciones/EjemplosArrays.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class EjemplosArrays {
 	//Coleccion de datos: LOS ARRAYS SON DE MEMORIA ESTÁTICA. 
 	//Si no sabemos cuantos datos tendremos, usamos listas
-	//Se usan mucho en INTELIGENCIA ARTIFICIAL
+	//Se usan mucho en INTELIGENCIA ARTIFICIAL y en JavaScript
 	
 	public static void nuevosEjemplos() {
 		int[] numeros = { 1, 2, 3, 4, 5 };
@@ -23,6 +23,7 @@ public class EjemplosArrays {
 		System.out.println("\n");
 		// suma
 		System.out.println(numeros[1] + "+" + numeros[3] + "=" + (numeros[1] + numeros[3]));
+		
 	}
 
 	private static void ejemplo1() {
@@ -59,6 +60,8 @@ public class EjemplosArrays {
 		for (String mes : meses) {
 			System.out.println(mes);
 		}
+		
+		System.out.println(String.join(", ", meses));
 
 		int numeroMes = 1;
 		for (String mes : meses) {
@@ -216,11 +219,44 @@ public class EjemplosArrays {
 
 	}
 
+	public static void redimensiones() {
+		//Existe ya una función de Java que genera una copia del array con un nuevo
+		//tamaño, por lo que si la utilizamos nos queda un código más compacto.
+		
+		String[] nombres = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+
+		nombres = Arrays.copyOf(nombres, 10); //agregamos nueva dimensión y se lo asignamos a nombres
+		nombres[5] = "Marta";
+		System.out.println(nombres.length); // 10
+		System.out.println(String.join(", ", nombres)); // Juan, Ana, Pedro, Eva, Paco, Marta, null, null
+		
+		for(String nombre : nombres) {
+			if(nombre!=null)
+				System.out.println(nombre + " tiene " + nombre.length() + " letras");
+		}
+	}
+	
+	public static void ejemploAumentarUno() {
+		Scanner sc = new Scanner(System.in);
+		String[] alumnos = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		System.out.println("Introduzca nuevo nombre del array:");
+		String alumno = sc.nextLine();
+		alumnos = Arrays.copyOf(alumnos, alumnos.length+1);
+		alumnos[alumnos.length-1] = alumno;
+		
+		System.out.println(String.join(", ", alumnos));
+		sc.close();
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		// ejemplo1();
+		//ejemplo1();
 		// buscarEnArray();
 		// nuevosEjemplos();
-		busquedaFuncional();
+		//busquedaFuncional();
+		//redimensiones();
+		ejemploAumentarUno();
 	}
 
 }
--- colecciones/src/colecciones/EjemplosListas.java
+++ colecciones/src/colecciones/EjemplosListas.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EjemplosListas {
-
+	//MEMORIA DINÁMICA
 	private static void ejemplo1() {
 		// Crear listas
 		List<String> nombres = new ArrayList<String>();
