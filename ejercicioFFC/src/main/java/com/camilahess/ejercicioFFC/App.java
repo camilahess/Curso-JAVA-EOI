d3ed505d5501bb98b83582df2d7e39a617ac51dc
Camila <114501675+camilahess@users.noreply.github.com>
2023-02-17 16:49:18 +0100

    Ultimos cambios

--- ejercicioFFC/src/main/java/com/camilahess/ejercicioFFC/App.java
+++ ejercicioFFC/src/main/java/com/camilahess/ejercicioFFC/App.java
@@ -17,106 +17,105 @@ import java.util.stream.Collectors;
 public class App {
 	public static void main(String[] args) {
 
-		// Definir la ruta donde se encuentran los archivos txt con la información de
-		// los clientes
+		// Definir la ruta donde se encuentran los archivos txt con la información de los clientes
 		final String ruta = "ficheros/";
+
+		// Leer la información de los clientes de los archivos txt y la almacena en una lista de objetos Cliente
+		List<Cliente> listaClientes = Cliente.leerClientes(ruta);
+
 		
-		// Pedir por consola el dni-cif del cliente
+		//MENÚ PARA PEDIR EL NÚMERO DNI
+		List<Cliente> clientesConMismoDniCif;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Introduce el dni-cif del cliente: ");
-		String dniCif = scanner.nextLine();
+		String dniCifIngresado;
+		do {
+		    System.out.println("Introduzca el dni-cif del cliente: ");
+		    dniCifIngresado = scanner.nextLine();
+		    if (dniCifIngresado.length() != 3) {
+		        System.out.println("DNI/CIF inválido. Por favor, introduce un DNI/CIF válido de 3 caracteres.");
+		    }
+		} while (dniCifIngresado.length() != 3);
 
-		// Leer la información de los clientes de los archivos txt y la almacena en una
-		// lista de objetos Cliente
-		List<Cliente> clientes = Cliente.leerClientes(ruta);
+		// Buscar en la lista de clientes el cliente con el dni-cif introducido por el usuario
+
+		String dniCliente = dniCifIngresado; //copio para poder utilizarlo en la función lambda
+		clientesConMismoDniCif = listaClientes.stream()
+				.filter(c -> c.getDniCif()
+				.equals(dniCliente))
+				.collect(Collectors.toList());
 		
-		// Buscar en la lista de clientes el cliente con el dni-cif introducido por el
-		// usuario
-		Cliente cliente = clientes.stream().filter(c -> c.getDniCif().equals(dniCif)).findFirst().orElse(null);
-//		List<Cliente> clientesConMismoDniCif = clientes.stream().filter(c -> c.getDniCif().equals(dniCif)).collect(Collectors.toList());
-//		System.out.println(clientesConMismoDniCif);
-		//Guardo en una lista los clientes con mismo DNI para comparar sus fechas de nacimiento
+		mostrarMensajeBienvenida(clientesConMismoDniCif.get(0)); //recibe un Cliente
+		buscarFechaNacimiento(clientesConMismoDniCif, dniCliente);
 		
-//		//PROBANDO STREAMS
-//		clientes.stream()
-//	            .filter(c -> c.getDniCif().equals(dniCif))
-//	            .map(Cliente::getFechaNacimiento)
-//	            .distinct()
-//	            .collect(Collectors.toList())
-//	            .forEach(System.out::println);
-		
-		// Mostrar el mensaje de bienvenida utilizando la función creada
-		mostrarMensajeBienvenida(cliente);
-		buscarFechaNacimiento(clientes, dniCif);
-		
+
 	}
-	
-	//Buscar fecha de nacimiento diferentes
-	
 
-//		private static void buscarFechaNacimiento(List<Cliente> clientes, String dniCif) {
-//		    List<Cliente> clientesConMismoDniCif = clientes.stream()
-//		        .filter(c -> c.getDniCif().equals(dniCif))
-//		        .collect(Collectors.toList());
-
+	private static void buscarFechaNacimiento(List<Cliente> clientesConMismoDniCif, String dniCliente) {
+//		List<LocalDate> fechasNacimiento = clientesConMismoDniCif.stream()
+//				.map(Cliente::getFechaNacimiento) // esto es lo mismo que c->c.getFechaNacimiento()
+//				.distinct()
+//				.collect(Collectors.toList());
 //
-//		    LocalDate fecha1 = clientesConMismoDniCif.get(0).getFechaNacimiento();
-//		    LocalDate fecha2 = clientesConMismoDniCif.get(1).getFechaNacimiento();
-//		    LocalDate fecha3 = clientesConMismoDniCif.get(2).getFechaNacimiento();
-//
-//		    if (!fecha1.equals(fecha2) || !fecha1.equals(fecha3)) {
-//		        System.out.println("Cuál es su fecha de nacimiento correcta? : ");
-//		        System.out.println("- " + fecha1);
-//		        System.out.println("- " + fecha2);
-//		        System.out.println("- " + fecha3);
-//		        System.out.println("Por favor, verifique y corrija los datos del cliente.");
-//		    }
-//		
+//		if (fechasNacimiento.size() == 1) {
+//			System.out.println("Fecha de nacimiento: " + fechasNacimiento.get(0));
+//		} else {
+//			System.out.println(
+//					"Tienes " + fechasNacimiento.size() + " fechas de nacimiento diferentes, elige la correcta:");
+//			for (int i = 0; i < fechasNacimiento.size(); i++) {
+//				System.out.println((i + 1) + ") " + fechasNacimiento.get(i));
+//			}
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.println((fechasNacimiento.size() == 2) ? "(1 o 2): " : "(1, 2 o 3): ");
+//			int opcion = scanner.nextInt();
+//			System.out.println("La fecha de nacimiento seleccionada es: " + fechasNacimiento.get(opcion - 1));
+//		}
 //	}
-	private static void buscarFechaNacimiento(List<Cliente> clientes, String dniCif) {
-		List<LocalDate> fechasNacimiento = clientes.stream()
-	            .filter(c -> c.getDniCif().equals(dniCif))
-	            .map(Cliente::getFechaNacimiento)
-	            .distinct()
-	            .collect(Collectors.toList());
-	            
-	        if (fechasNacimiento.size() == 1) {
-	            System.out.println("Fecha de nacimiento: " + fechasNacimiento.get(0));
-	        } else {
-	            System.out.println("Tienes " + fechasNacimiento.size() + " fechas de nacimiento diferentes, elige la correcta:");
-	            for (int i = 0; i < fechasNacimiento.size(); i++) {
-	                System.out.println((i+1) + ") " + fechasNacimiento.get(i));
-	            }
-	            Scanner scanner = new Scanner(System.in);
-	            System.out.println((fechasNacimiento.size() == 2)? "(1 o 2): " : "(1, 2 o 3): ");
-	            int opcion = scanner.nextInt();
-	            System.out.println("La fecha de nacimiento seleccionada es: " + fechasNacimiento.get(opcion - 1));
-	        }
-	    }
+
+		    List<LocalDate> fechasNacimiento = clientesConMismoDniCif.stream()
+		            .map(Cliente::getFechaNacimiento)
+		            .distinct()
+		            .collect(Collectors.toList());
+
+		    String mensaje1 = "";
+		    String mensaje2 = "";
+		    if (clientesConMismoDniCif.stream().anyMatch(c -> c.getCodigoPais().equals("ES"))) {
+		        mensaje1 = "Fecha de nacimiento: ";
+		        mensaje2 = "Tienes " + fechasNacimiento.size() + " fechas de nacimiento diferentes, elige la correcta:";
+		    } else {
+		        mensaje1 = "Date of birth: ";
+		        mensaje2 = "You have " + fechasNacimiento.size() + " different dates of birth, choose the correct one:";
+		    }
+
+		    if (fechasNacimiento.size() == 1) {
+		        System.out.println(mensaje1 + fechasNacimiento.get(0));
+		    } else {
+		        System.out.println(mensaje2);
+		        for (int i = 0; i < fechasNacimiento.size(); i++) {
+		            System.out.println((i + 1) + ") " + fechasNacimiento.get(i));
+		        }
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.println((fechasNacimiento.size() == 2) ? "(1 or 2): " : "(1, 2 or 3): ");
+		        int opcion = scanner.nextInt();
+		        System.out.println(mensaje1 + fechasNacimiento.get(opcion - 1));
+		    }
+		}
+
+
+
 
 
 	// Función para mostrar el mensaje de bienvenida
 	private static void mostrarMensajeBienvenida(Cliente cliente) {
-//		String mensajeBienvenida = "";
-//		if (cliente == null) {
-//			mensajeBienvenida = "Lo siento, no se ha encontrado un cliente con ese dni-cif.";
-//		} else {
 
-			if (cliente.getCodigoPais().equals("ES")) {
-				System.out.println(
-						"Bienvenid@ " + cliente.getNombre() + "!\n" + getFormatoFecha(cliente.getCodigoPais()));
+		if (cliente.getCodigoPais().equals("ES")) {
+			System.out.println("Bienvenid@ " + cliente.getNombre() + "!\n" + getFormatoFecha(cliente.getCodigoPais()));
 
-			} else {
-				System.out.println("Welcome " + cliente.getNombre() + "!\n" + getFormatoFecha(cliente.getCodigoPais()));
-			}
+		} else {
+			System.out.println("Welcome " + cliente.getNombre() + "!\n" + getFormatoFecha(cliente.getCodigoPais()));
 		}
-		//System.out.println(mensajeBienvenida);
-//}
+	}
 
-	//MOSTRAR FECHA DE NACIMIENTO
-	
-	
-	
+
 	// Función que devuelve el patrón de formato de fecha adecuado según el código
 	// de país del cliente
 	private static String getFormatoFecha(String codigoPais) {
@@ -128,8 +127,5 @@ public class App {
 					DateTimeFormatter.ofPattern("'Date: 'eeee',' MMMM dd',' yyyy\n'Time: 'HH:mm:ss", Locale.UK));
 		}
 	}
-
-
-
 
 }
--- ejercicioFFC/src/main/java/com/camilahess/ejercicioFFC/Producto.java
+++ ejercicioFFC/src/main/java/com/camilahess/ejercicioFFC/Producto.java
@@ -1,6 +1,6 @@
 package com.camilahess.ejercicioFFC;
 
-import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
