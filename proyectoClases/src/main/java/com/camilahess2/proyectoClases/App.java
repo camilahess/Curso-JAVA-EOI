0eb23bfd0a1847480eed1d71306a8b9d94fa1693
Camila <114501675+camilahess@users.noreply.github.com>
2023-02-23 11:48:10 +0100

    Actualización con instrucciones de cada ejercicio

--- proyectoClases/src/main/java/com/camilahess2/proyectoClases/App.java
+++ proyectoClases/src/main/java/com/camilahess2/proyectoClases/App.java
@@ -1,6 +1,5 @@
 package com.camilahess2.proyectoClases;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -13,37 +12,75 @@ import com.camilahess2.proyectoClases.metodos.Metodos;
 
 public class App {
 	private static final String RUTA = "ficheros/";
-	
-	
+
 	public static void main(String[] args) {
 
 		List<CuentaCaixa> cuentasCaixa = CuentaCaixa.listaCaixa(RUTA);
 		List<CuentaSabadell> cuentasSabadell = CuentaSabadell.listaSabadell(RUTA);
 		List<CuentaSantander> cuentasSantander = CuentaSantander.listaSantander(RUTA);
-		
-		
+
 		List<Cuenta> todasLasCuentas = Metodos.unirListas(cuentasCaixa, cuentasSabadell, cuentasSantander);
-		
+
 		for (Cuenta cuenta : todasLasCuentas) {
-		    if (cuenta instanceof CuentaCaixa) {
-		        System.out.println("Cuenta CAIXA: " + cuenta);
-		    } else if (cuenta instanceof CuentaSabadell) {
-		        System.out.println("Cuenta SABADELL: " + cuenta);
-		    } else if (cuenta instanceof CuentaSantander) {
-		        System.out.println("Cuenta SANTANDER: " + cuenta);
-		    }
+			if (cuenta instanceof CuentaCaixa) {
+				System.out.println("Cuenta CAIXA: " + cuenta);
+			} else if (cuenta instanceof CuentaSabadell) {
+				System.out.println("Cuenta SABADELL: " + cuenta);
+			} else if (cuenta instanceof CuentaSantander) {
+				System.out.println("Cuenta SANTANDER: " + cuenta);
+			}
 		}
-		
-	
+
 		// Borra un elemento al azar de la lista todasLasCuentas
 		Cuenta cuentaEliminada = Metodos.borrarCuentaAzar(todasLasCuentas);
 		System.out.println("\nLa cuenta eliminada es:\n" + cuentaEliminada);
-		
-		//SANTANDER 
+
+		// SANTANDER
 		System.out.println("Números de cuentas del banco Santander: " + CuentaSantander.getNumCuentas());
-		System.out.println("Suma de todos los saldos del banco Santander: " + CuentaSantander.getSumaSaldos(cuentasSantander) + "€\n");
+		System.out.println("Suma de todos los saldos del banco Santander: "
+				+ CuentaSantander.getSumaSaldos(cuentasSantander) + "€\n");
 		System.out.println("Cuenta con el saldo máximo: \n" + CuentaSantander.getCuentaConSaldoMax(cuentasSantander));
-		
-		
+
 	}
 }
+
+/*
+ * A partir de los archivos de los ficheros dados, realiza las siguientes tareas:
+ * 
+ * 1. Crea una clase genérica Cuenta que no se pueda instanciar con los campos
+ * comunes de todos los archivos. Crea todos los métodos básicos de dicha
+ * cuenta.
+ * 
+ * 2. Crea tantas clases heredadas como de bancos tienes información. Llámalas
+ * CuentaSantander ...
+ * 
+ * 3. En las Clases de bancos catalanes (caixa y sabadell) cada vez que se cree
+ * una cuenta deberá introducirse el nivel de catalán que tiene el titular de la
+ * cuenta admitiendo solo tres opciones (alto, medio o bajo).
+ * 
+ * 4. En las cuentas del banco Santander queremos saber si el titular de la
+ * cuenta es residente en Santander. Además queremos un contador interno en la
+ * clase que almacene el número de cuentas que se crean de ese banco.
+ * 
+ * 5. Las cuentas de bancos catalanes no pueden ser heredadas por ninguna otra
+ * clase.
+ * 
+ * 6. Crea un toString diferente para cada Cuenta de banco.
+ * 
+ * 7. Crea un proceso que lea los ficheros y cree una lista de objetos por cada
+ * uno de los tipos de cuenta.
+ * 
+ * 8. Une todas las listas en una sola lista.
+ * 
+ * 9. Recorre esa lista agrupada y antes de mostrar sus datos indica el banco al
+ * que pertenece.
+ * 
+ * 10. Crea un método que borre al azar un elemento de esta nueva lista.
+ * 
+ * 11. Busca en las listas de las cuentas de los diferentes bancos para
+ * descubrir cuál de sus elementos no está en la lista agrupada.
+ * 
+ * 12. Sobre la lista del banco Santander saca la suma total de los saldos de
+ * todos los clientes, el número de Cuentas que hay y aquella Cuenta con el
+ * saldo más alto.
+ */
--- proyectoFFC/src/main/java/com/camilahess/proyectoFFC/App.java
+++ proyectoFFC/src/main/java/com/camilahess/proyectoFFC/App.java
@@ -7,31 +7,73 @@ import java.util.Scanner;
 
 public class App {
 	public static final String RUTA = "ficheros/";
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 
 		List<Cliente> listaClientes = Cliente.leerClientes(RUTA);
 
 		List<Producto> listaProductos = Producto.leerProductos(RUTA);
-		
+
 		List<Cliente> clientesEncontrados = null;
-		String dniCifIngresado="";
+		String dniCifIngresado = "";
 		while (clientesEncontrados == null || dniCifIngresado.length() != 3) {
-		    Scanner sc = new Scanner(System.in);
-		    System.out.println("Introduzca el dni-cif del cliente: ");
-		    dniCifIngresado = sc.nextLine();
-		    clientesEncontrados = Metodos.buscarClientePorDniIngresado(listaClientes, dniCifIngresado);
-		    if (dniCifIngresado.length() != 3 || clientesEncontrados == null ) {
-		        System.out.println("DNI/CIF inválido o no encontrado. Por favor, introduce un DNI/CIF válido de 3 caracteres.");
-		    }
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Introduzca el dni-cif del cliente: ");
+			dniCifIngresado = sc.nextLine();
+			clientesEncontrados = Metodos.buscarClientePorDniIngresado(listaClientes, dniCifIngresado);
+			if (dniCifIngresado.length() != 3 || clientesEncontrados == null) {
+				System.out.println(
+						"DNI/CIF inválido o no encontrado. Por favor, introduce un DNI/CIF válido de 3 caracteres.");
+			}
 		}
-	
+
 		String codigo = Metodos.codigoPais(clientesEncontrados.get(0));
 		Metodos.mostrarMensajeBienvenida(clientesEncontrados.get(0), codigo); // recibe un Cliente
 		LocalDate fecha = Metodos.buscarFechaNacimiento(clientesEncontrados, dniCifIngresado, codigo);
 		int edad = Period.between(fecha, LocalDate.now()).getYears();
-		System.out.println(Metodos.recomendarProducto(edad, Metodos.saldoTotal(clientesEncontrados), listaProductos, codigo));
+		System.out.println(
+				Metodos.recomendarProducto(edad, Metodos.saldoTotal(clientesEncontrados), listaProductos, codigo));
 
 	}
 }
+
+/*
+ * Somos una entidad financiera que trabaja con varios bancos y que quiere
+ * ofrecer productos a sus clientes dependiendo de ciertas características del
+ * mismo. Se pedirá por consola el dni-cif del cliente, a partir de ese momento
+ * buscaremos en una ruta (que tendremos definida como una variable global)
+ * todos los ficheros txt que recogen información de los clientes en los
+ * diferentes bancos. Hemos conseguido que todos los bancos nos envíen toda la
+ * información en un formato unificado de este tipo:
+ * 
+ * dni-cif;nombre_cliente;fecha_nacimiento(dd/mm/aaaa);código_país;saldo
+ * 
+ * El programa al recibir un dni-cif hará lo siguiente:
+ * 
+ * Sacar un mensaje de bienvenida en el idioma del cliente (en español para los
+ * clientes de España y en inglés para el resto) donde incluya su nombre (puedes
+ * coger el primero que encuentres).
+ * 
+ * La hora actual (en formato "largo", y poniendo el día antes que el mes para
+ * los clientes españoles y el mes antes que el día para el resto)
+ * 
+ * Posteriormente, comprobará si el cliente tiene más de una fecha de nacimiento
+ * en los ficheros, en cuyo caso, sacará por pantalla las mismas para que el
+ * cliente seleccione cuál es la correcta (si solo hay una no preguntará).
+ * 
+ * Una vez confirmada la fecha de nacimiento, buscaremos en otro fichero de
+ * productos (donde tenemos criterios de que productos ofrecer basándonos en el
+ * saldo y la edad del cliente) acorde al siguiente formato:
+ * 
+ * edad_mínima;edad_maxima;saldo_mínimo;saldo_máximo;nombre_producto
+ * 
+ * (si el cliente puede acceder a más de un producto financiero, mostraremos
+ * aquel cuyo saldo mínimo sea mayor). Para obtener el saldo del cliente deberás
+ * sumar (puede haber saldos negativos), su saldo de todos los bancos.
+ * 
+ * Nota: Intenta estructurar en clases el proyecto Maven. Utiliza las clases ya
+ * vistas en el curso hasta el momento.
+ * 
+ * Se adjuntan datos de 3 bancos (caixa, santander y sabadell) y el fichero de
+ * productos (productosofertados)
+ */
