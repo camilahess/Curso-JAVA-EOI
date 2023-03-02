f4f54d32cbc4e389c834684b2c6392995dac0f08
Camila <114501675+camilahess@users.noreply.github.com>
2023-03-02 10:06:14 +0100

    Campos de bbdd completos con Prepared Statement

--- harrypotter/src/main/java/com/camilahess/harrypotter/App.java
+++ harrypotter/src/main/java/com/camilahess/harrypotter/App.java
@@ -1,9 +1,12 @@
 package com.camilahess.harrypotter;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -79,29 +82,35 @@ public class App
 	}
 	
 	public static void poblarbBbdd() {
-		String sql = "";
-		Statement st;
-		con = null;
-		try {
-			con = DriverManager.getConnection(URI, USER, PASSWORD);
-			st = con.createStatement();
-			
-			for(CharacterHP personaje: personajes) {
-				sql ="INSERT INTO personaje(id,name) VALUES('" +
-						personaje.getId() + "','" + 
-						personaje.getName() + 
-					"');";
-				st.executeUpdate(sql); //Por duplicidad de clave primaria no podríamos hacerlo dos veces
-				
-			}
-		
-			st.close();
-			con.close();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Fallo en la conexión");
-		}
+	    String sql = "INSERT INTO personaje(id, name, species, gender, house, dateOfBirth, yearOfBirth, wizard, ancestry, eyeColour, hairColour, patronus, hogwartsStudent, hogwartsStaff, actor, alive, image) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	    try (Connection con = DriverManager.getConnection(URI, USER, PASSWORD);
+	         PreparedStatement ps = con.prepareStatement(sql)) {
+
+	        for (CharacterHP personaje : personajes) {
+	            ps.setString(1, personaje.getId());
+	            ps.setString(2, personaje.getName());
+	            ps.setString(3, personaje.getSpecies());
+	            ps.setString(4, personaje.getGender());
+	            ps.setString(5, personaje.getHouse());
+	            ps.setObject(6, personaje.getDateOfBirthLD());
+	            ps.setInt(7, personaje.getYearOfBirth() != null ? personaje.getYearOfBirth() : 0);
+	            ps.setBoolean(8, personaje.getWizard());
+	            ps.setString(9, personaje.getAncestry());
+	            ps.setString(10, personaje.getEyeColour());
+	            ps.setString(11, personaje.getHairColour());
+	            ps.setString(12, personaje.getPatronus());
+	            ps.setBoolean(13, personaje.getHogwartsStudent());
+	            ps.setBoolean(14, personaje.getHogwartsStaff());
+	            ps.setString(15, personaje.getActor());
+	            ps.setBoolean(16, personaje.getAlive());
+	            ps.setString(17, personaje.getImage());
+	            ps.executeUpdate();
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        System.out.println("Fallo en la conexión");
+	    }
 	}
 	
     public static void main( String[] args )
